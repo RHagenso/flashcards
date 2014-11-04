0589287481c0f59603c58bf445df09a93815975a
RHagenso <rhagenso@nebrwesleyan.edu>
2014-11-04 09:26:19 -0600

    Even more minor edit (fixed typo)

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 var questions = [
     {
-	question: 'Is this going to be enough to create a webpage when I commit',
+	question: 'Is this going to be enough to create a webpage when I commit?',
 	options: ['This choice', 'This other choice', 'This yet another choice'],
 	answer: ['This choice'],
     },
