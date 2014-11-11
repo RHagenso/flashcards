317a687d065655f7b93d7fee24230c971df0f032
pherink <patrickherink9@gmail.com>
2014-11-11 10:03:39 -0500

    added categories to question and also added Id's to html

--- questions.js
+++ questions.js
@@ -3,46 +3,55 @@ var questions = [
 	question: 'Is this going to be enough to create a webpage when I commit?',
 	options: ['This choice', 'This other choice', 'This yet another choice'],
 	answer: ['This choice'],
-	category: ['Category 1']
+	category: ['Category 1'],
     },
     {
 	question: 'What is the answer to this second question?',
 	options: ['This choice', 'This other choice', 'This yet another choice'],
 	answer: ['This choice'],
+	category: ['Category 2'],
     },
     {
 	question: 'What is the answer to this third question?',
 	options: ['This choice', 'This other choice', 'This yet another choice'],
 	answer: ['This choice'],
+	category: ['Category 1'],
     },
     {
 	question: 'What is the answer to this fourth question?',
 	options: ['This choice', 'This other choice', 'This yet another choice'],
 	answer: ['This choice'],
+	category: ['Category 2'],
     },
     {
 	question: 'What is the answer to this fifth question?',
 	options: ['This choice', 'This other choice', 'This yet another choice'],
 	answer: ['This choice'],
+	category: ['Category 1'],
     },
     {
 	question: 'What is the answer to this sixth question?',
 	options: ['This choice', 'This other choice', 'This yet another choice'],
 	answer: ['This choice'],
+	category: ['Category 2'],
     },
     {
 	question: 'What is the answer to this seventh question?',
 	options: ['This choice', 'This other choice', 'This yet another choice'],
 	answer: ['This choice'],
+	category: ['Category 1'],
     },
     {
 	question: 'What is the answer to this eighth question?',
 	options: ['This choice', 'This other choice', 'This yet another choice'],
 	answer: ['This choice'],
+	category: ['Category 1'],
     },
     {
 	question: 'What is the answer to this ninth question?',
 	options: ['This choice', 'This other choice', 'This yet another choice'],
 	answer: ['This choice'],
+	category: ['Category 2'],
     },
 ];
+document.getElementById("answers").innerHTML = question[0].options 
