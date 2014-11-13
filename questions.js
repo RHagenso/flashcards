5dc805d81632e0073f6a01fad3000f747e386a5e
RHagenso <rhagenso@nebrwesleyan.edu>
2014-11-13 10:25:46 -0500

    Took out JS loop for now

--- questions.js
+++ questions.js
@@ -56,9 +56,7 @@ var questions = [
 ];
 
 var i = 0;
-while (i < questions.length) {
 	document.getElementById("option0").innerHTML = questions[i].options[0];
 	document.getElementById("option1").innerHTML = questions[i].options[1];
 	document.getElementById("option2").innerHTML = questions[i].options[2];
 	document.getElementById("question").innerHTML = questions[i].question;
-}
