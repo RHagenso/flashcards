1d9dc19954fae34d7c088e88d8808e2a49503395
RHagenso <rhagenso@nebrwesleyan.edu>
2014-11-20 10:08:11 -0500

    added percentage, disabled buttons after one click

--- questions.js
+++ questions.js
@@ -6,22 +6,10 @@ var questions = [
 	category: ['Category 1'],
     },
     {
-	question: 'Who would win in a fight, Batman or Superman?',
-	options: ['Batman', 'Superman', 'The Flash'],
-	answer: ['Superman'],
-	category: ['Category 2'],
-    },
-    {
 	question: 'What is "The T in a classroom?',
 	options: ['Are we talking Mr. T?', 'We do not drink T in America', 'The first row and middle section of desks'],
 	answer: ['The first row and middle section of desks'],
 	category: ['Category 1'],
-    },
-    {
-	question: 'How does the Hulk not rip his pants?',
-	options: ['Super glue', 'Stretchy pants', 'The world may never know'],
-	answer: ['The world may never know'],
-	category: ['Category 2'],
     },
     {
 	question: 'In a programming class ";" is a semicolon, what is it in a Greek language class?',
@@ -30,22 +18,10 @@ var questions = [
 	category: ['Category 1'],
     },
     {
-	question: 'Is Deadpool awesome?',
-	options: ['No', 'Yes', 'Fried chicken'],
-	answer: ['Yes'],
-	category: ['Category 2'],
-    },
-    {
 	question: 'Math exams cause test anxiety, therefore test axiety causes?',
 	options: ['Crying students', 'The world to end', 'Math tests'],
 	answer: ['Crying students'],
 	category: ['Category 1'],
-    },
-    {
-	question: 'Wolverine, what country is he from?',
-	options: ['USA!', 'Canada, ay', 'Down under (Australia)'],
-	answer: ['Canada, ay'],
-	category: ['Category 2'],
     },
     {
 	question: 'What should you modify if something is not present on a webpage?',
@@ -53,16 +29,44 @@ var questions = [
 	answer: ['HTML'],
 	category: ['Category 1'],
     },
+    {
+	question: 'Who would win in a fight, Batman or Superman?',
+	options: ['Batman', 'Superman', 'The Flash'],
+	answer: ['Superman'],
+	category: ['Category 2'],
+    },
+    {
+	question: 'How does the Hulk not rip his pants?',
+	options: ['Super glue', 'Stretchy pants', 'The world may never know'],
+	answer: ['The world may never know'],
+	category: ['Category 2'],
+    },
+    {
+	question: 'Is Deadpool awesome?',
+	options: ['No', 'Yes', 'Fried chicken'],
+	answer: ['Yes'],
+	category: ['Category 2'],
+    },
+    {
+	question: 'Wolverine, what country is he from?',
+	options: ['USA!', 'Canada, ay', 'Down under (Australia)'],
+	answer: ['Canada, ay'],
+	category: ['Category 2'],
+    },
 ];
 
 var i = 0;
+var j = 0;
 function next() {
 	i = i+0.5;
 	document.getElementById("option0").innerHTML = questions[i].options[0];
 	document.getElementById("option1").innerHTML = questions[i].options[1];
 	document.getElementById("option2").innerHTML = questions[i].options[2];
 	document.getElementById("question").innerHTML = questions[i].question;
-	document.getElementById("correct_or_no").innerHTML = i;
+	document.getElementById("correct_or_no").innerHTML = "Question number is "+i;
+	document.getElementById("button0").addEventListener('click',correct_or_not0);
+	document.getElementById("button1").addEventListener('click',correct_or_not1);
+	document.getElementById("button2").addEventListener('click',correct_or_not2);
 }
 
 document.getElementById('next_button').addEventListener('click',next);
@@ -70,32 +74,39 @@ document.getElementById('next_button').addEventListener('click',next);
 function correct_or_not0() {
 	if (questions[i].answer[0] === document.getElementById("option0").innerHTML) {
 		document.getElementById("correct_or_no").innerHTML = "Correct!";
+		j = j+1;
 	}
 	else {
 		document.getElementById("correct_or_no").innerHTML = "Incorrect!";
 	}
+	document.getElementById("button0").removeEventListener('click',correct_or_not0);
+	document.getElementById("button1").removeEventListener('click',correct_or_not1);
+	document.getElementById("button2").removeEventListener('click',correct_or_not2);
 }
 
 function correct_or_not1() {
 	if (questions[i].answer[0] === document.getElementById("option1").innerHTML) {
 		document.getElementById("correct_or_no").innerHTML = "Correct!";
+		j = j+1;
 	}
 	else {
 		document.getElementById("correct_or_no").innerHTML = "Incorrect!";
 	}
+	document.getElementById("button0").removeEventListener('click',correct_or_not0);
+	document.getElementById("button1").removeEventListener('click',correct_or_not1);
+	document.getElementById("button2").removeEventListener('click',correct_or_not2);
 }
 
 function correct_or_not2() {
 	if (questions[i].answer[0] === document.getElementById("option2").innerHTML) {
 		document.getElementById("correct_or_no").innerHTML = "Correct!";
+		j = j+1;
 	}
 	else {
 		document.getElementById("correct_or_no").innerHTML = "Incorrect!";
 	}
+	document.getElementById("button0").removeEventListener('click',correct_or_not0);
+	document.getElementById("button1").removeEventListener('click',correct_or_not1);
+	document.getElementById("button2").removeEventListener('click',correct_or_not2);
+	
 }
-
-document.getElementById("button0").addEventListener('click',correct_or_not0);
-document.getElementById("button1").addEventListener('click',correct_or_not1);
-document.getElementById("button2").addEventListener('click',correct_or_not2);
-
-
